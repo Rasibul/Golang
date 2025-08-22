f6aacaef3f6d274c85d85354a9d1fa22ef881210
Rasibul <rasibul179@gmail.com>
2025-08-22 19:02:45 +0600

    variable showdown

--- first.go
+++ first.go
@@ -3,20 +3,32 @@ package main
 import "fmt"
 
 
-var a int = 10
-var b int = 20
+// var a int = 10
+// var b int = 20
 
-// Correct type is int, not init
-func printf(num int) {
-	fmt.Println("The number is:", num)
-}
+// // Correct type is int, not init
+// func printf(num int) {
+// 	fmt.Println("The number is:", num)
+// }
 
-func add(a int, b int) int {
-	res := a + b
-	printf(res)
-	return res
-}
+// func add(a int, b int) int {
+// 	res := a + b
+// 	printf(res)
+// 	return res
+// }
 
-func main() {
-	add(a, b)
+// func main() {
+// 	add(a, b)
+//
+
+var  a int = 10
+
+func main(){
+	age := 20
+	if age > 18 {
+		a:=47
+		fmt.Println(a)
+	}
+
+	fmt.Println(a)
 }
